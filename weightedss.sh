75245a40846b1a95bf50faeb9b8a938798ee0b5d
RichD <richd44@gmail.com>
2011-04-03 05:57:30 -0700

    Added copyright and GPL License statement

--- weightedss.sh
+++ weightedss.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 # Weighted Slideshow
 #
+# Copyright 2011 by RichD (richd44@gmail.com)
+# Released under the GNU General Public License
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# ---------------------------------------------------------------------------------------------------------------------
+#
 # Creates a playlist file from image files in the current directory.
-#	Edit the first line below to change default types: *.jpg *.png *.gif *.tif
 #	The images included in the playlist are randomly selected using an algorithm
 #	that strongly favors newer images, i.e., the newer the image, the higher 
 #	its chance of being chosen.
 #	The playlist file can then be used as input to your favorite utility.
+#	Edit the first line below to change default types: *.jpg *.png *.gif *.tif
 #
 #	Usage: weightedss.sh filename count
 #		where:
